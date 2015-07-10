7fcdd81bca075d12a87506f21ee00efcb8b5c2c9
David Tolpin <david.tolpin@gmail.com>
2015-07-10 13:25:12 +0100

    updated anglican dependency

--- project.clj
+++ project.clj
@@ -5,7 +5,7 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [nstools "0.2.4"]
-                 [anglican "0.6.6-SNAPSHOT"]]
+                 [anglican "0.7.0"]]
   :plugins [[dtolpin/lein-gorilla "0.3.5-SNAPSHOT"]]
   :resource-paths ["programs"]
   :main ^:skip-aot anglican.core)

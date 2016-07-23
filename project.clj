95b0ed62890534dfb67cfdcddbf832344f45dcfe
Jan-Willem van de Meent <jwvdm@robots.ox.ac.uk>
2016-07-23 12:25:44 +0100

    updated to latest anglican version (1.0.0) and clojure (1.8.0)

--- project.clj
+++ project.clj
@@ -3,9 +3,9 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.6.0"]
+  :dependencies [[org.clojure/clojure "1.8.0"]
                  [nstools "0.2.4"]
-                 [anglican "0.7.0"]]
+                 [anglican "1.0.0"]]
   :plugins [[dtolpin/lein-gorilla "0.3.5-SNAPSHOT"]]
   :resource-paths ["programs"]
   :main ^:skip-aot anglican.core)

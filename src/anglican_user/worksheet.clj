102288d03a21b8143fd0de29d617501efcf6cc2c
David Tolpin <david.tolpin@gmail.com>
2018-05-14 00:24:15 +0300

    removed things which should not be there

--- src/anglican_user/worksheet.clj
+++ src/anglican_user/worksheet.clj
@@ -1,4 +1,4 @@
 (ns anglican-user.worksheet
   (:require [gorilla-plot.core :as plot])
   (:use [anglican core emit runtime
-         [state :only [get-predicts get-log-weight]]]))
+         [state :only [get-predicts get-log-weight get-result]]]))

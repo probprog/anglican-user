58133de27f509794f12ad4b63be6c8180fcdd752
Seonho Kim <ksseono@gmail.com>
2020-09-14 23:45:52 +0900

    update to the latest version of clojure and anglican

--- project.clj
+++ project.clj
@@ -3,9 +3,9 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url  "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.8.0"]
+  :dependencies [[org.clojure/clojure "1.10.1"]
                  [nstools "0.2.4"]
-                 [anglican "1.0.0"]]
+                 [anglican "1.1.0"]]
   :plugins [[org.clojars.benfb/lein-gorilla "0.7.0]]
   :resource-paths ["programs"]
   :main ^:skip-aot anglican.core)

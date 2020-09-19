6e1be81ffe9e52a7bbb190ec7b3e9b0bce905588
Seonho Kim <ksseono@gmail.com>
2020-09-19 13:15:54 +0900

    fix typo

--- project.clj
+++ project.clj
@@ -6,6 +6,6 @@
   :dependencies [[org.clojure/clojure "1.10.1"]
                  [nstools "0.2.4"]
                  [anglican "1.1.0"]]
-  :plugins [[org.clojars.benfb/lein-gorilla "0.7.0]]
+  :plugins [[org.clojars.benfb/lein-gorilla "0.7.0"]]
   :resource-paths ["programs"]
   :main ^:skip-aot anglican.core)
